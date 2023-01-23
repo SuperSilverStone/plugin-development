9680d458cfd287694af59dea7104ad4c57c930da
SuperSilverStone <88118771+SuperSilverStone@users.noreply.github.com>
2023-01-22 21:29:14 -0500

    Create PluginTutorial.java

---
+++ episode3/PluginTutorial.java
@@ -0,0 +1,25 @@
+package me.supersilverstone.plugintutorial;
+
+import me.supersilverstone.plugintutorial.episode2.events;
+import me.supersilverstone.plugintutorial.episode3.playerListener;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PluginTutorial extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        // Episode 2
+        Bukkit.getPluginManager().registerEvents(new events(), this);
+
+        // Episode 3
+        Bukkit.getPluginManager().registerEvents(new playerListener(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
